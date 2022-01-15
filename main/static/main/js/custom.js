b8a4de4d5bcbd64c8bf0825dc3d061dfd7070fce
Elisha <elishae621@gmail.com>
2022-01-15 17:17:59 +0100

    add alertify to project

--- main/static/main/js/custom.js
+++ main/static/main/js/custom.js
@@ -1,6 +1,29 @@
+alertify.set('notifier', 'position', 'bottom-right');
+
+if ($('.d-none.message').length) {
+    const message = $('.d-none.message');
+    alertify.notify(message.text(), message.data('status'), 10)
+}
+
+
 if (window.location.hash == '#cart') {
     $('.cart-btn').click()
 }
+
 $('.icofont-logout').on('click', function () {
     $('form.logout-form').submit();
 });
+
+var subscribe = function (pk) {
+    $.ajax({
+        url: $('.news-form').attr('data-url'),
+        method: 'POST',
+        data: {
+            'csrfmiddlewaretoken': $("[name='csrfmiddlewaretoken']").val(),
+            'email': $('.sub-email').val()
+        },
+        success: function (data) {
+            alertify.alert(data.message);
+        }
+    })
+}
